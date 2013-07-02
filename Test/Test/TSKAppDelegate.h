da1b1c88ad6b7d66612b134e31208962edfab078
Sergii Kutnii <mnkutster@gmail.com>
2013-07-02 19:16:39 +0300

    Project created

---
+++ Test/Test/TSKAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TSKAppDelegate.h
+//  Test
+//
+//  Created by Serge Kutny on 7/2/13.
+//  Copyright (c) 2013 42coffeecups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSKAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
